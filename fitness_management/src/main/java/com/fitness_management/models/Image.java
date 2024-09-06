7fe66405112bfa5003a40998a96ba9b6d1829e2b
Reda Ouadi <redaouadi34@gmail.com>
2024-09-06 22:11:43 +0100

    Ajouter classe Image pour stocker les images  dans cloudinary

---
+++ fitness_management/src/main/java/com/fitness_management/models/Image.java
@@ -0,0 +1,23 @@
+package com.fitness_management.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor @NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String imageUrl;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = true)
+    private User user;
+}

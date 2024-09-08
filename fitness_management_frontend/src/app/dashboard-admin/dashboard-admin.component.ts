9351cf5695d58f15293a9c7ebf53e7f715358a5b
Reda Ouadi <redaouadi34@gmail.com>
2024-09-08 22:25:42 +0100

    creation de admin-dashbord

---
+++ fitness_management_frontend/src/app/dashboard-admin/dashboard-admin.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { PersonneService } from '../services/personne.service';
+
+@Component({
+  selector: 'app-dashboard-admin',
+  templateUrl: './dashboard-admin.component.html',
+  styleUrls: ['./dashboard-admin.component.css']
+})
+export class DashboardAdminComponent implements OnInit {
+  totalUsers: number = 0;
+  totalCoaches: number = 0;
+
+  constructor(private personneService: PersonneService) {}
+
+  ngOnInit(): void {
+    this.loadCounts();
+  }
+
+  private loadCounts() {
+    this.personneService.getTotalUsers().subscribe(count => this.totalUsers = count);
+    this.personneService.getTotalCoaches().subscribe(count => this.totalCoaches = count);
+  }
+}

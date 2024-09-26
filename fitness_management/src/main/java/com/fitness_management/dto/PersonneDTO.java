cb6812f972379986fa1d2743c3ae0ef22e732eb6
Reda Ouadi <redaouadi34@gmail.com>
2024-09-26 11:40:42 +0100

    Ajouter PersonneDTO en Angular

--- fitness_management/src/main/java/com/fitness_management/dto/PersonneDTO.java
+++ fitness_management/src/main/java/com/fitness_management/dto/PersonneDTO.java
@@ -1,15 +1,31 @@
 package com.fitness_management.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PersonneDTO {
     private Long id;
     private String nom;
-    private String prenom;
+    private String prenom;  // Changed to camelCase
     private String email;
+    private String motDePasse;  // If needed, consider omitting this in DTO
     private String username;
+    private String genre;  // Adjusted for camelCase
     private String role;
+    private ImageDTO profileImage;
+
+
+
+    public void setRole(String role) {
+        this.role = role.toUpperCase().startsWith("ROLE_") ? role : "ROLE_" + role.toUpperCase();
+    }
 }
+
